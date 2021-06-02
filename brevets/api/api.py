6364ec275e8b3c0164a6d60cb24c05d7da2aa811
Chris Cortes <cortes.d.chris@gmail.com>
2021-06-02 01:29:09 -0700

    Fixed api.py, almost

--- brevets/api/api.py
+++ brevets/api/api.py
@@ -7,47 +7,54 @@ from pymongo import MongoClient
 
 import json
 
-from bson import json_util
+from bson.json_util import loads, dumps
+
 
 app = Flask(__name__)
 api = Api(app)
 
 client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
-db = client["database"]
-collection = db["brevtimes"]
+db = client.tododb
 
 
 class listAll(Resource):
     def get(self, dtype=""):
-        top = request.args.get("k", type=int)
-        times = list(collection.find({},{"_id":0, "open": 1, "close": 1 }))
+        times = list(db.tododb.find({},{"_id":0, "open": 1, "close": 1 }))
         if dtype == 'json':
-            return json.loads(json_util.dumps(times))
+            return loads(dumps(times))
         if dtype == 'csv':
-            for x in collection.find({},{"_id":0, "open": 1, "close": 1 }):
-                return json.loads(x)
+            # cant get into csv form
+            return loads(dumps(times))
 
 
 class listOpenOnly(Resource):
     def get(self, dtype=""):
-        top = request.args.get("k", type=int)
-        times = list(collection.find({},{"_id":0, "open": 1 }))
+        top = request.args.get('top', type=int)
+        app.logger.debug(top)
+        times = db.tododb.find({},{"_id":0, "open": 1 })
+        # cant get k to work
+        # times = db.tododb.find({},{"_id":0, "open": 1 }).limit(top)
+        # times = times[:top]
         if dtype == 'json':
-            return json.loads(json_util.dumps(times))
+            return loads(dumps(times))
         if dtype == 'csv':
-            for x in collection.find({},{"_id":0, "open": 1 }):
-                return json.loads(x)
+            # cant get into csv form
+            return loads(dumps(times))
 
 
 class listCloseOnly(Resource):
     def get(self, dtype=""):
-        top = request.args.get("k", type=int)
-        times = list(collection.find({},{"_id":0, "close": 1 }))
+        top = request.args.get('top', type=int)
+        app.logger.debug(top)
+        times = list(db.tododb.find({},{"_id":0, "close": 1 }))
+        # cant get k to work
+        # times = db.tododb.find({},{"_id":0, "close": 1 }).limit(top)
+        # times = times[:top]
         if dtype == 'json':
-            return json.loads(json_util.dumps(times))
+            return loads(dumps(times))
         if dtype == 'csv':
-            for x in collection.find({},{"_id":0, "close": 1 }):
-                return json.loads(x)
+            # cant get into csv form
+            return loads(dumps(times))
 
 
 # Create routes
